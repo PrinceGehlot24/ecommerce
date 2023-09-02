85231e76c0c11080900911235a9a48222d450392
PrinceGehlot24 <princegehlot24@gmail.com>
2023-09-02 12:09:28 +0530

    add customer, prodcuct, order, cart models

--- app/models.py
+++ app/models.py
@@ -1,3 +1,66 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-# Create your models here.
+STATE_CHOICES = (
+    ("Andhra Pradesh", "Andhra Pradesh"),
+    ("Arunachal Pradesh", "Arunachal Pradesh"),
+    ("Uttar Pradesh", "Uttar Pradesh"),
+    ("Himachal Pradesh", "Himachal Pradesh"),
+    ("Madhya Pradesh", "Madhya Pradesh"),
+)
+
+CATEGORY_CHOICES = (
+    ('M', 'Mobile'),
+    ('L', 'Laptop'),
+    ('TW', 'Top Wear'),
+    ('BW', 'Bottom Wear'),
+)
+
+STATUS_CHOICES = (
+    ('Accepted','Accepted'),
+    ('Packed','Packed'),
+    ('On the Way','On the Way'),
+    ('Delivered','Delivered'),
+    ('Cancel','Cancel'),
+)
+
+class Customer(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    locality = models.CharField(max_length=200)
+    city = models.CharField(max_length=50)
+    zipcode = models.IntegerField()
+    state = models.CharField(choices=STATE_CHOICES, max_length=50)
+
+    def __str__(self):
+        return str(self.id)
+
+class Product(models.Model):
+    title = models.CharField(max_length=100)
+    selling_price = models.FloatField()
+    discounted_price = models.FloatField()
+    description = models.TextField()
+    brand = models.CharField(max_length=100)
+    category = models.CharField(choices=CATEGORY_CHOICES, max_length=2)
+    product_image = models.ImageField(upload_to='productimg')
+
+    def __str__(self):
+        return str(self.id)
+
+class Cart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return str(self.id)
+
+class OrderPlaced(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+    ordered_date = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=50, choices=STATUS_CHOICES, default='Pending')
+    
+
